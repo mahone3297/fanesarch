17ecebe51d3d52d2fd02fe106baa91cff2a90d4b
mahone3297 <329730566@qq.com>
2013-11-30 12:23:44 +0800

    rails new fanesarch --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fanesarch::Application.config.secret_key_base = '9574b82371f74c62b82a0f9d88c36294074c3111b5a1e941cdd14c790439788bf2ac43929ed4c4bd9b272270bbc4a7a245574abfc3f6115de60ecf9d4b0c4193'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fanesarch::Application.config.session_store :cookie_store, key: '_fanesarch_session'
