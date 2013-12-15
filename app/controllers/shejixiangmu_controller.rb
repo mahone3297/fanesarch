24ed16a0337c59db78896fd60028226920ea9016
mahone3297 <329730566@qq.com>
2013-12-15 20:26:16 +0800

    uniform app/views/shejixiangmu/*.html.erb to app/views/shejixiangmu/list.html.erb

--- app/controllers/shejixiangmu_controller.rb
+++ app/controllers/shejixiangmu_controller.rb
@@ -6,37 +6,55 @@ class ShejixiangmuController < ApplicationController
 
   # 居住地产
   def juzhudichan
+    @num_array = (1..17).to_a
+    render 'list'
   end
 
   # 综合商业
   def zongheshangye
+    @num_array = (1..22).to_a
+    render 'list'
   end
 
   # 酒店度假
   def jiudiandujia
+    @num_array = (1..4).to_a
+    render 'list'
   end
 
   # 办公研发
   def bangongyanfa
+    @num_array = (1..11).to_a
+    render 'list'
   end
 
   # 展示会所
   def zhanshihuisuo
+    @num_array = (1..5).to_a
+    render 'list'
   end
 
   # 教育科研
   def jiaoyukeyan
+    @num_array = (1..9).to_a
+    render 'list'
   end
 
   # 工业建筑
   def gongyejianzhu
+    @num_array = (1..11).to_a
+    render 'list'
   end
 
   # 室内设计
   def shineisheji
+    @num_array = (1..6).to_a
+    render 'list'
   end
 
   # 景观设计
   def jingguansheji
+    @num_array = (1..9).to_a
+    render 'list'
   end
 end
