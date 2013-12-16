0111eb77d6ee10a2d0b9bdfc192f433e64d4dd2f
mahone3297 <329730566@qq.com>
2013-12-16 23:49:04 +0800

    uniform shejiyanjiu jiajujichanpin,yanjiulunwen,jianzhuhua to shejiyanjiu/index.html.erb

--- app/controllers/shejiyanjiu_controller.rb
+++ app/controllers/shejiyanjiu_controller.rb
@@ -1,13 +1,16 @@
 class ShejiyanjiuController < ApplicationController
   # 家具及产品
   def jiajujichanpin
+    render 'index'
   end
 
   # 研究论文
   def yanjiulunwen
+    render 'index'
   end
 
   # 建筑画
   def jianzhuhua
+    render 'index'
   end
 end
