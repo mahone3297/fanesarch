9b80cba84351136c070bcfb25834bfb8388399cc
mahone3297 <329730566@qq.com>
2013-12-23 17:27:00 +0800

    rename slidesjs -> slidesjs3, 明确表明使用的是 3.0 版本

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap/bootstrap.min
-//= require slidesjs/jquery.slides.min
+//= require slidesjs3/jquery.slides.min
 //= require shejixiangmu
 // require_tree .
