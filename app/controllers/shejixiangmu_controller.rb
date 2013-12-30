3e91b04b6cc597935a8ddb7e8eb89b4902daaeb2
mahone3297 <329730566@qq.com>
2013-12-30 22:01:43 +0800

    detail page, add category

--- app/controllers/shejixiangmu_controller.rb
+++ app/controllers/shejixiangmu_controller.rb
@@ -66,20 +66,23 @@ class ShejixiangmuController < ApplicationController
   def detail
     @category = params[:category]
     @idx = params[:idx]
-    case @idx
-    when '1'
-      @text =<<EOF
+    case @category
+    when 'juzhudichan'
+      case @idx
+      when '1'
+        @text =<<EOF
 项目名称 东湖林语（规划、建筑）
 项目规模 占地53360平方米，建筑面积70000平方米
 项目状况 建成
 坐落  湖北武汉东湖路368号
 EOF
-      @num_array = (1..8).to_a
-      @image_path = @category + '/detail/1武汉东湖林语/'
-    else
-      @text = ''
-      @num_array = []
-      @image_path = ''
+        @num_array = (1..8).to_a
+        @image_path = @category + '/detail/1武汉东湖林语/'
+      else
+        @text = ''
+        @num_array = []
+        @image_path = ''
+      end
     end
     render 'detail'
   end
